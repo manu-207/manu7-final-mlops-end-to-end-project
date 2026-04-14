61f10d58316a6b83e8941ef281e411e30e2b8714
manu-207 <117650246+manu-207@users.noreply.github.com>
2026-04-14 20:52:08 +0530

    mlflow ip change

--- src/train.py
+++ src/train.py
@@ -11,7 +11,7 @@ from urllib.parse import urlparse
 
 import mlflow
 
-MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://52.66.41.165:5000")
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://65.2.177.69:5000")
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment(os.getenv("MLFLOW_EXPERIMENT_NAME", "manu7-mlops"))
 
